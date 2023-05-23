9b684d5d14da9f7745ce75c6fb7ec3b7ccd6f0c7
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-05-23 23:28:18 +0200

    Maven - pom.xml and test

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloTest {
+    @Test
+    public void test1() {
+
+        assertTrue (true);
+    }
+}
